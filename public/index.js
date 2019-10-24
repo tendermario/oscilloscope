13e48e319671cd5120ab08282385c9762785acb8
Mitchell Burton <mitch@mitchellburton.ca>
2019-10-24 11:56:59 -0700

    modularize audio file into Oscillator class

--- public/index.js
+++ public/index.js
@@ -1,65 +1,71 @@
 import * as THREE from 'three'
 
-import {analyser, dataArray, oscillator} from './audio'
+import Oscilloscope from './audio'
+
+const DEFAULT_FREQS = ['240']
+
+const CAMERA_ARGS = [75, window.innerWidth/window.innerHeight, 0.1, 1000]
+const CAMERA_Z_POS = 10
+const RENDERER_SIZE = [window.innerWidth, window.innerHeight]
+const GEOMETRY = [2, 2, 2]
+const MESH_MAT = {color: 0xfd58d7}
+const LIGHT_COLOR = 0xffff00
+const LIGHT_POS = [10, 0, 25]
 
 document.addEventListener('DOMContentLoaded', () => {
+  // 0. Create the oscilloscope
+  const oscilloscope = new Oscilloscope(DEFAULT_FREQS)
+
   // 1. Create the scene
   const scene = new THREE.Scene()
 
   // 2. Add a camera view
-  const camera = new THREE.PerspectiveCamera(
-    75, window.innerWidth/window.innerHeight, 0.1, 1000)
+  const camera = new THREE.PerspectiveCamera(...CAMERA_ARGS)
 
-  camera.position.z = 10
+  camera.position.z = CAMERA_Z_POS
 
   // 3. Add a renderer to generate the camera's picture
   const renderer = new THREE.WebGLRenderer()
-  renderer.setSize( window.innerWidth, window.innerHeight )
+  renderer.setSize(...RENDERER_SIZE)
 
   // 4. Attach what we're rendering to the web page
   document.body.appendChild(renderer.domElement)
 
   // 5. Lighting + Objects
-  const geometry = new THREE.BoxGeometry(2, 2, 2)
-  const material = new THREE.MeshLambertMaterial({color: 0xfd59d7})
+  const geometry = new THREE.BoxGeometry(...GEOMETRY)
+  const material = new THREE.MeshLambertMaterial(MESH_MAT)
   const cube = new THREE.Mesh(geometry, material)
   scene.add(cube)
 
-  const light = new THREE.PointLight(0xffff00)
-  light.position.set(10, 0, 25)
+  const light = new THREE.PointLight(LIGHT_COLOR)
+  light.position.set(...LIGHT_POS)
   scene.add(light)
 
-  const updateObjects = () => {
+  const updateObjects = dataArray => {
     position++
 
     if (position >= dataArray.length) {
       position = 0
     }
 
+    if (dataArray.length === 0) {
+      return
+    }
+
     cube.rotation.x = dataArray[position] / 100
     // cube.rotation.y += .01
   }
 
-  let x = 100
-  const doOnceASecond = (cb) => {
-    if (--x < 1) {
-      x = 100
-      cb()
-    }
-  }
-
-  let position = 0;
+  let position = 0
+  let dataArray = []
 
   // 6. Run the renderer
   const animate = () => {
     requestAnimationFrame(animate)
 
-    // Audio
-
-    doOnceASecond(() => console.log(dataArray))
 
     // Visual
-    updateObjects()
+    updateObjects(dataArray)
     camera.updateProjectionMatrix()
     renderer.render(scene, camera)
   }
@@ -70,9 +76,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
   startButton.addEventListener('mousedown', () => {
     // Make sound
-    oscillator.start()
+    oscilloscope.start()
+
     setTimeout(() => {
-      analyser.getByteTimeDomainData(dataArray)
+      dataArray = oscilloscope.getByteTimeDomainData()[0]
     }, 1000);
     startButton.remove();
   })
