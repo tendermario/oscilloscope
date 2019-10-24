cca7607a225279f36249ec4adb105506c23e88f0
Mitchell Burton <mitch@mitchellburton.ca>
2019-10-23 21:35:25 -0700

    added buggy button

--- public/index.js
+++ public/index.js
@@ -1,55 +1,64 @@
-// 1. Create the scene
-const scene = new THREE.Scene()
+document.addEventListener('DOMContentLoaded', () => {
+  // 1. Create the scene
+  const scene = new THREE.Scene()
 
-// 2. Add a camera view
-const camera = new THREE.PerspectiveCamera(
-  75, window.innerWidth/window.innerHeight, 0.1, 1000)
+  // 2. Add a camera view
+  const camera = new THREE.PerspectiveCamera(
+    75, window.innerWidth/window.innerHeight, 0.1, 1000)
 
-camera.position.z = 10
+  camera.position.z = 10
 
-// 3. Add a renderer to generate the camera's picture
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize( window.innerWidth, window.innerHeight )
+  // 3. Add a renderer to generate the camera's picture
+  const renderer = new THREE.WebGLRenderer()
+  renderer.setSize( window.innerWidth, window.innerHeight )
 
-// 4. Attach what we're rendering to the web page
-document.body.appendChild(renderer.domElement)
+  // 4. Attach what we're rendering to the web page
+  document.body.appendChild(renderer.domElement)
 
-// 5. Lighting + Objects
-const geometry = new THREE.BoxGeometry(2, 2, 2)
-const material = new THREE.MeshLambertMaterial({color: 0xfd59d7})
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
+  // 5. Lighting + Objects
+  const geometry = new THREE.BoxGeometry(2, 2, 2)
+  const material = new THREE.MeshLambertMaterial({color: 0xfd59d7})
+  const cube = new THREE.Mesh(geometry, material)
+  scene.add(cube)
 
-const light = new THREE.PointLight(0xffff00)
-light.position.set(10, 0, 25)
-scene.add(light)
+  const light = new THREE.PointLight(0xffff00)
+  light.position.set(10, 0, 25)
+  scene.add(light)
 
-const updateObjects = () => {
-  cube.rotation.x += .01
-  cube.rotation.y += .01
-}
-
-let x = 100
-const doOnceASecond = (cb) => {
-  if (--x < 1) {
-    x = 100
-    cb()
+  const updateObjects = () => {
+    cube.rotation.x += .01
+    cube.rotation.y += .01
   }
-}
 
+  let x = 100
+  const doOnceASecond = (cb) => {
+    if (--x < 1) {
+      x = 100
+      cb()
+    }
+  }
 
-// 6. Run the renderer
-const animate = () => {
-  requestAnimationFrame(animate)
+  // 6. Run the renderer
+  const animate = () => {
+    requestAnimationFrame(animate)
 
-  // Audio
-  analyser.getByteTimeDomainData(dataArray)
-  doOnceASecond(() => console.log(dataArray))
+    // Audio
+    analyser.getByteTimeDomainData(dataArray)
+    doOnceASecond(() => console.log(dataArray))
 
-  // Visual
-  updateObjects()
-  camera.updateProjectionMatrix()
-  renderer.render(scene, camera)
-}
+    // Visual
+    updateObjects()
+    camera.updateProjectionMatrix()
+    renderer.render(scene, camera)
+  }
 
-animate()
+  animate()
+
+  const startButton = document.querySelector('#start')
+
+  startButton.addEventListener('mousedown', () => {
+    // Make sound
+    oscillator.start()
+    startButton.remove();
+  })
+})
